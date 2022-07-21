b1a7a19815439020b174e7314ba60b46610e0640
doBoi <maulana4810@gmail.com>
2022-07-21 19:56:02 +0700

    finish migrate camp, campBenefit and Checkout.

---
+++ database/migrations/2022_07_21_122701_create_camp_benefits_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Constraint\Constraint;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            // 1st method add foreign key
+            // $table->bigInteger('camp_id')->unsigned();
+
+            //2nd method add foreign key
+            $table->foreignId('camp_id')->constrained();
+            $table->string('name');
+            $table->timestamps();
+            // 1st method add foreign key
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+};
